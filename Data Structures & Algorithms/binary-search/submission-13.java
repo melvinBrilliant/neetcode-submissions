dfc4a4010fcf981eaacfc6f6d0c999373786ba64
melvin.brilliant <brilliantmelvin@gmail.com>
2026-08-24 02:38:38 +0700

    Add: binary-search - submission-13

---
+++ Data Structures & Algorithms/binary-search/submission-13.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        // if (target > nums[nums.length - 1])
+        //     return -1;
+        int l = 0;
+        int r = nums.length - 1;
+        while (l <= r) {
+            int m = (l + r) / 2;
+            int mid = nums[m];
+            if (mid == target) {
+                return m;
+            } else if (mid > target) {
+                r = m - 1;
+            } else {
+                l = m + 1;
+            }
+        }
+        return -1;
+    }
+}

eda458c2760e121eafdecdc67656da5e01944b0e
melvin.brilliant <brilliantmelvin@gmail.com>
2026-09-09 17:04:35 +0700

    Add: minimum-stack - submission-24

---
+++ Data Structures & Algorithms/minimum-stack/submission-24.java
@@ -0,0 +1,31 @@
+class MinStack {
+    Deque<Integer> stack;
+    Deque<Integer> minStack;
+
+    public MinStack() {
+        stack = new ArrayDeque<>();
+        minStack = new ArrayDeque<>();
+    }
+    
+    public void push(int val) {
+        stack.push(val);
+        if (minStack.isEmpty())
+            minStack.push(val);
+        else
+            minStack.push(
+                Math.min(minStack.peek(), val));
+    }
+    
+    public void pop() {
+        stack.pop();
+        minStack.pop();
+    }
+    
+    public int top() {
+        return stack.peek();
+    }
+    
+    public int getMin() {
+        return minStack.peek();
+    }
+}

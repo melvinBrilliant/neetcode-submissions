63ec1def6fa24f4ebee211fbdfe2fa7792cd9e87
melvin.brilliant <brilliantmelvin@gmail.com>
2026-09-13 04:46:07 +0700

    Add: three-integer-sum - submission-58

---
+++ Data Structures & Algorithms/three-integer-sum/submission-58.js
@@ -0,0 +1,32 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[][]}
+     */
+    threeSum(nums) {
+        nums.sort((a, b) => a - b);
+        let res = [];
+        for (let i = 0; i < nums.length; i++) {
+            if (i != 0 && nums[i] === nums[i - 1]) {
+                continue;
+            }
+            let j = i + 1;
+            let k = nums.length - 1;
+            let target = -1 * nums[i];
+            while (j < k) {
+                let sum = nums[j] + nums[k];
+                if (sum === target) {
+                    res.push([nums[i], nums[j], nums[k]]);
+                    j++; k--;
+                    while (j < k && nums[j] === nums[j - 1]) j++;
+                    while (j < k && nums[k] === nums[k + 1]) k--;
+                } else if (sum < target) {
+                    j++;
+                } else {
+                    k--;
+                }
+            }
+        }
+        return res;
+    }
+}

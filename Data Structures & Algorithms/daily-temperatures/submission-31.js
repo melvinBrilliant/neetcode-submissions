7f52974965f549d04693fea36afe4c9d3b89f90e
melvin.brilliant <brilliantmelvin@gmail.com>
2026-09-14 16:20:03 +0700

    Add: daily-temperatures - submission-31

---
+++ Data Structures & Algorithms/daily-temperatures/submission-31.js
@@ -0,0 +1,20 @@
+class Solution {
+    /**
+     * @param {number[]} temperatures
+     * @return {number[]}
+     */
+    dailyTemperatures(temperatures) {
+        const n = temperatures.length;
+        let res = new Array(n).fill(0);
+        let stack = [];
+        for (let i = 0; i < n; i++) {
+            // inline condition: for better performance
+            while (!(stack.length === 0) && temperatures[i] > temperatures[stack[stack.length - 1]]) {
+                const prev = stack.pop();
+                res[prev] = i - prev;
+            }
+            stack.push(i);
+        }
+        return res;
+    }
+}

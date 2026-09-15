75fd31c9e80d9247b79003b3654f222228bc6f60
melvin.brilliant <brilliantmelvin@gmail.com>
2026-09-15 17:22:26 +0700

    Add: car-fleet - submission-3

---
+++ Data Structures & Algorithms/car-fleet/submission-3.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int carFleet(int target, int[] position, int[] speed) {
+        Map<Integer, Integer> map = new HashMap<>();
+        int n = position.length;
+        for (int i = 0; i < n; i++)
+            map.put(position[i], speed[i]);
+        Arrays.sort(position);
+        ArrayDeque<Integer> stack = new ArrayDeque<>();
+        int fleets = 0;
+        double maxTime = 0.0;
+        for (int i = n - 1; i >= 0; i--) {
+            int pos = position[i];
+            double time = (double) (target - pos) / map.get(pos);
+            if (time > maxTime) {
+                fleets++;
+                maxTime = time;
+            }
+        }
+        return fleets;
+    }
+}

40bfaa5af42958038f3c22849bbf8733457971cd
melvin.brilliant <brilliantmelvin@gmail.com>
2026-09-18 02:57:43 +0700

    Add: find-minimum-in-rotated-sorted-array - submission-15

---
+++ Data Structures & Algorithms/find-minimum-in-rotated-sorted-array/submission-15.js
@@ -0,0 +1,10 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    findMin(nums) {
+        // joke solution
+        return Math.min(...nums);
+    }
+}

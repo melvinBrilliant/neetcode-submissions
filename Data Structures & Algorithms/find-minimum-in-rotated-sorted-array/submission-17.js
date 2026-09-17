d99e01b5448bf34a35c73464d3e9f058b8b19597
melvin.brilliant <brilliantmelvin@gmail.com>
2026-09-18 02:59:29 +0700

    Add: find-minimum-in-rotated-sorted-array - submission-17

---
+++ Data Structures & Algorithms/find-minimum-in-rotated-sorted-array/submission-17.js
@@ -0,0 +1,20 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    findMin(nums) {
+        let l = 0;
+        let r = nums.length - 1;
+        while (l < r) {
+            if (nums[l] <= nums[r]) return nums[l];
+            const mid = Math.floor((l + r) / 2);
+            if (nums[mid] >= nums[l]) {
+                l = mid + 1;
+            } else {
+                r = mid;
+            }
+        }
+        return nums[l];
+    }
+}
